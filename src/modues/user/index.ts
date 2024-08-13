76927343da611628a75a788f0feb7f78c7a816c9
CamelJohn <jonathan23986@gmail.com>
2024-08-13 19:18:18 +0300

    Rename middleware

--- src/modues/user/index.ts
+++ src/modues/user/index.ts
@@ -1,21 +1,21 @@
 import { Router } from 'express';
 import user_controller from './controller';
 import validate from './validation.middleware';
-import { user_does_not_exist, user_exists } from './middleware';
+import { user_should_exist, user_should_not_exist } from './middleware';
 
 const user_routes = Router();
 
 user_routes.post(
     '/sign-up',
     validate.sign_up_request,
-    user_exists, 
+    user_should_not_exist, 
     user_controller.sign_up
 );
 
 user_routes.post(
     '/sign-in',
     validate.sign_in_request,
-    user_does_not_exist,
+    user_should_exist,
     user_controller.sign_in
 );
 
