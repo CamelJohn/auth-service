99aa15b502a1c19807ad190a71cf5e9b67ab4f0e
CamelJohn <jonathan23986@gmail.com>
2024-08-15 22:47:37 +0300

    Cleanup code

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,16 +1,10 @@
 import db_connection from './connection';
 
 const db = {
-    $connect: async () => {
-        await db_connection.sync();
-    },
-
-    $disconnect: async () => {
-        await db_connection.close();
-    },
-    $test_connection: async () => {
-        await db_connection.authenticate();
-    }
+    connection: db_connection,
+    $connect: () => db_connection.sync(),
+    $disconnect: () => db_connection.close(),
+    $test_connection: () => db_connection.validate(),
 }
 
 export default db;
